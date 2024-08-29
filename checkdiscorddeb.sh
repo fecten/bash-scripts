7861c5eb692c518fe6cdf6277736bce118979b55
fecten <adamalymalick@gmail.com>
2024-08-29 13:38:43 -0400

    if cond, and kill and restart of the discord service

--- checkdiscorddeb.sh
+++ checkdiscorddeb.sh
@@ -17,6 +17,38 @@ wget -O $DISCORD_DOWNLOAD $DISCORD_URL
 #Get the version of discord downloaded .deb file
 LATEST_VERSION=$(dpkg-deb -I $DISCORD_DOWNLOAD | grep Version | awk '{print $2}')
 
+
+if [ "$LATEST_VERSION" != "$CURRENT_VERSION" ]; then
+    echo "New Discord version detected: $LATEST_VERSION"
+
+    #Run the updatediscord script
+    bash $HOME/Documents/proj_scripts/bash_scripts/updatediscord.sh
+
+    #Kill Discord
+    #Specify -15 to gracfully kill discord SIGTERM(15)
+    #SIGTERM (15): Gracefully terminate.
+    #SIGKILL (9): Forcefully kill immediately.
+    #SIGINT (2): Interrupt interactively.
+    #SIGHUP (1): Hang up or reload.
+    #SIGQUIT (3): Quit with core dump.
+    #SIGSTOP (19): Pause.
+    #SIGCONT (18): Resume.
+    #SIGUSR1/2: Custom signals for user-defined actions.
+    killall -15 Discord
+
+    #Need to restart discord without a hangup and have it run in the background such that it does not interrupt the current shell.
+    #So we use nohup
+    #discord to specify the service
+    #&> redirect stdout and stderr to a file or device in this case /dev/null
+    # & at the end runs it in the background.
+    nohup discord &>/dev/null &
+else
+    echo "Discord is up to date."
+
+fi
+
+#Clean up
+rm $DISCORD_DOWNLOAD
 echo "URL:$DISCORD_URL PATHtoVer:$path_to_version_info CurrentV:$CURRENT_VERSION DownloadLOC:$DISCORD_DOWNLOAD LatestV:$LATEST_VERSION"
 
 
